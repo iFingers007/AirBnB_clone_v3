fff7027c3814ee28238cfaecbdea3fb243103a24
Nkeiru Lois <nkeiruloisy.com>
2024-08-05 04:51:43 +0000

    Add JSON-formatted 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """ Flask Application """
 from flask import Flask, jsonify, make_response
 from models import storage
+from os import getenv
+from flask_cors import CORS
 from api.v1.views import app_views
-from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def close_db(error):
@@ -19,6 +21,6 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
