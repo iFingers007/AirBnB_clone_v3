36c4ae193e5c40046693a4cdbe0b30cf4ed1b748
Abu Jehoiada <abujehoiada@gmail.com>
2024-08-05 21:33:28 +0000

    Fixed pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,15 +10,18 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error """
     return make_response(jsonify({'error': "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
